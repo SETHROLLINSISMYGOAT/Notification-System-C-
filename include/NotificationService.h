9780250066bf8b95ffeb6a3b8a1663a876f76f57
Siddhant <siddhantpathak128@gmail.com>
2026-05-18 16:58:02 +0530

    Changes

---
+++ include/NotificationService.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <memory>
+#include "NotificationStrategy.h"
+#include "NotificationObservable.h"
+class NotificationService : public NotificationObservable{
+private:
+    shared_ptr<NotificationStrategy> strategy;
+public:
+    NotificationService(shared_ptr<NotificationStrategy> strategy) : strategy(strategy){}
+    void sendNotification(string msg){
+        strategy->send(msg);
+        notifyObservers(msg);
+    }
+
+};
